da7fd2af09beb7af2a03c39191450295ff474773
Damoon02 <alanlunab02@gmail.com>
2026-07-04 14:21:30 -0600

    Agregamos el listado y formulario de libros

--- libros/views.py
+++ libros/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Libro
+from .forms import LibroForm
+
+
+def lista_libros(request):
+    libros = Libro.objects.all()
+
+    busqueda = request.GET.get('buscar')
+
+    if busqueda:
+        libros = libros.filter(titulo__icontains=busqueda)
+
+    return render(request, 'libros/lista_libros.html', {
+        'libros': libros
+    })
+
+
+def crear_libro(request):
+    if request.method == 'POST':
+        form = LibroForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('lista_libros')
+    else:
+        form = LibroForm()
+
+    return render(request, 'libros/formulario_libro.html', {
+        'form': form,
+        'titulo': 'Registrar libro'
+    })
+
+
+def editar_libro(request, libro_id):
+    libro = get_object_or_404(Libro, id=libro_id)
+
+    if request.method == 'POST':
+        form = LibroForm(request.POST, instance=libro)
+
+        if form.is_valid():
+            form.save()
+            return redirect('lista_libros')
+    else:
+        form = LibroForm(instance=libro)
+
+    return render(request, 'libros/formulario_libro.html', {
+        'form': form,
+        'titulo': 'Editar libro'
+    })
+
+
+def eliminar_libro(request, libro_id):
+    libro = get_object_or_404(Libro, id=libro_id)
+
+    libro.estado = 'BAJA'
+    libro.save()
+
+    return redirect('lista_libros')
